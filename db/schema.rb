f250f20cfdfee4401e7f294f134c126981463cd1
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-03-04 17:19:45 +0100

    Generating blog via scaffold.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_153436) do
+ActiveRecord::Schema.define(version: 2020_03_04_161750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
